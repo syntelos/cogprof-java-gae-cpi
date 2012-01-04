f63744020c0283f8efb81cc03ddd6cf7c45beec8
jdp.ulsf.net <jdp.ulsf.net@da6cfb42-1c7d-6735-6cf8-8ec2ebf6b9e3>
2012-01-04 00:37:09 +0000

    cpi 3.1.1 -- init odl

---
+++ server/src/cpi/groups/ProjectServlet.java
@@ -0,0 +1,31 @@
+
+package cpi.groups;
+
+import oso.data.Person;
+import cpi.Redirect;
+import cpi.Margins;
+
+import gap.*;
+import gap.data.*;
+import gap.util.*;
+
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.blobstore.*;
+
+import java.util.Date;
+
+/**
+ * Generated once.  This source file will not be overwritten
+ * unless deleted, so it can be edited.
+ *
+ * @see Project
+ */
+public final class ProjectServlet
+    extends gap.service.Servlet
+{
+
+    public ProjectServlet() {
+        super();
+    }
+
+}
