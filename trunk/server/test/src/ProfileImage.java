c0494fecb48e512eef89091a20314e0a90719d97
john.douglas.pritchard <john.douglas.pritchard@da6cfb42-1c7d-6735-6cf8-8ec2ebf6b9e3>
2012-02-14 00:33:44 +0000

    cpi 3.1.2 -- testing font variations

--- trunk/server/test/src/ProfileImage.java
+++ trunk/server/test/src/ProfileImage.java
@@ -8,12 +8,14 @@ public class ProfileImage
 
     private final static cpi.ProfileLabels ProfileLabels1 = new cpi.ProfileLabels();
     private final static cpi.ProfileLabels ProfileLabels2 = new cpi.ProfileLabels("Friendly Sporty Smart Funny");
+    private final static cpi.ProfileLabels ProfileLabels3 = new cpi.ProfileLabels("The+Storyteller The+Organizer The+Creative The+Problem+Solver");
+
+    private final static String ProfileFontLat15 = "Lat15-Terminus12x6.psfu";
 
 
     public ProfileImage(){
         super();
     }
-
 
     public void testProfileImage1(){
         try {
@@ -21,15 +23,7 @@ public class ProfileImage
 
             java.io.File out = new java.io.File("testProfileImage1.png");
 
-            byte[] png = image.toPNG();
-            java.io.OutputStream os = new java.io.FileOutputStream(out);
-            try {
-                os.write(png,0,png.length);
-                os.flush();
-            }
-            finally {
-                os.close();
-            }
+            ProfileImage.Write(image,out);
 
             assertTrue(true);
         }
@@ -46,15 +40,7 @@ public class ProfileImage
 
             java.io.File out = new java.io.File("testProfileImage2.png");
 
-            byte[] png = image.toPNG();
-            java.io.OutputStream os = new java.io.FileOutputStream(out);
-            try {
-                os.write(png,0,png.length);
-                os.flush();
-            }
-            finally {
-                os.close();
-            }
+            ProfileImage.Write(image,out);
 
             assertTrue(true);
         }
@@ -65,4 +51,37 @@ public class ProfileImage
             assertTrue(false);
         }
     }
+    public void testProfileImage3(){
+        try {
+            cpi.ProfileImage image = new cpi.ProfileImage(1.0f,0.5f,0.3f,0.5f,ProfileLabels3,ProfileFontLat15);
+
+            java.io.File out = new java.io.File("testProfileImage.png");
+
+            ProfileImage.Write(image,out);
+
+            assertTrue(true);
+        }
+        catch (java.io.IOException exc){
+
+            exc.printStackTrace();
+
+            assertTrue(false);
+        }
+    }
+
+
+    private static void Write(cpi.ProfileImage image, java.io.File out)
+        throws java.io.IOException
+    {
+        byte[] png = image.toPNG();
+        java.io.OutputStream os = new java.io.FileOutputStream(out);
+        try {
+            os.write(png,0,png.length);
+            os.flush();
+        }
+        finally {
+            os.close();
+        }
+    }
 }
+
